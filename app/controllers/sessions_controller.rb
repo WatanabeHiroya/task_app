1110675c01939d69769ccef7da8576ad4c2c1f11
EC2 Default User <ec2-user@ip-172-31-42-122.ap-northeast-1.compute.internal>
2019-11-16 08:37:12 +0000

    D5-1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_or_back user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
@@ -19,5 +19,16 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
+  end
+  
+  # 記憶しているURL(またはデフォルトURL)にリダイレクトします。
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶します。
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :update]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
   
   def show
   end
@@ -31,9 +32,23 @@ class UsersController < ApplicationController
     end
   end
   
+  def index
+    @users = User.all
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
+    def logged_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+      end
+    end
+    
+    
 end
